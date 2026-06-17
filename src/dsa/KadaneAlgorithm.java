d5287d39bd51e40edb63802efb7999816a04c09e
Shabuddin Shaik <78738399+Shabuddin27@users.noreply.github.com>
2026-06-18 01:05:12 +0530

    Add Kadane's solution.

--- src/dsa/KadaneAlgorithm.java
+++ src/dsa/KadaneAlgorithm.java
@@ -2,13 +2,14 @@ package dsa;
 
 public class KadaneAlgorithm {
     public static void main(String[] args){
-        int[] arr={-2,-100};
-         int sum=arr[0];
-         int max=arr[0];
-         for(int i=1;i<arr.length;i++){
-             sum =Math.max(arr[i],sum+arr[i]);
-             max =Math.max(sum,max);
-         }
-        System.out.println(max);
+        int[] arr={1, 2, 3, -2, 5};
+        int sum = arr[0];
+        int best = arr[0];
+        for(int i=1;i<arr.length;i++){
+            sum = Integer.max(arr[i],sum+arr[i]);
+            best = Integer.max(best,sum);
+            System.out.println("sum is --> "+sum+" "+"best is --> "+best);
+        }
+        System.out.println(best);
             }
 }
